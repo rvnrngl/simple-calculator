04350245c7e6452cfc2a2aca65ad723deae74c2d
Reevss <0217.raven.ringel@gmail.com>
2023-06-20 22:06:22 +0800

    fix: some errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,54 @@
 import { useState } from "react";
+import { evaluate } from "mathjs";
 import "./App.css";
 
 function App() {
   const [display, setDisplay] = useState("0");
 
-  const handleNumbers = (event) => {
-    const number = event.target.textContent;
-    if (display === "0" || display === 0) {
-      setDisplay(number);
+  const handleDisplay = (value) => {
+    if (/[/+*]/.test(display[display.length - 1]) && value.match(/[/+*]/)) {
+      setDisplay((ex) => ex.slice(0, -1) + value);
+    } else if (
+      /[-]/.test(display[display.length - 1]) &&
+      value.match(/[/+*]/)
+    ) {
+      setDisplay((ex) => ex.slice(0, -2) + value);
+    } else if (/--/.test(display)) {
+      setDisplay((ex) => ex.slice(0, -2) + "+" + value);
+    } else if (/[.]/.test(display[display.length - 1]) && value.match(/[.]/)) {
+      setDisplay((ex) => ex + "");
+    } else if (
+      /\d[.]/g.test(display) &&
+      /\d[.]\d+[/+*-]\d+$/g.test(display) === false &&
+      value.match(/[.]/)
+    ) {
+      setDisplay((ex) => ex + "");
+    } else if (/\d[.]\d+[/+*-]\d+$/g.test(display) && value.match(/[.]/)) {
+      setDisplay((ex) => ex + value);
+    } else if (display === "0" && value === "0") {
+      setDisplay((ex) => ex + "");
+    } else if (display === "0" && value.match(/[^.=]/)) {
+      setDisplay(value);
     } else {
-      setDisplay(display + number);
+      setDisplay((ex) => ex + value);
     }
-  };
 
-  const handleOperators = (event) => {
-    const operator = " " + event.target.textContent + " ";
-    if (display !== "0") {
-      const array = display.split(" ");
-      const lastElement = array[array.length - 1];
-      if (isNaN(parseInt(lastElement)) === false) {
-        setDisplay(display + operator);
+    if (display[display.length - 1] === "=") {
+      if (/[0-9.]/.test(value)) {
+        setDisplay(value);
+      } else {
+        setDisplay(answer + value);
       }
     }
   };
 
-  const handleEqual = () => {
-    setDisplay(parseFloat(eval(display).toFixed(12)).toString());
-  };
-
-  const handleDecimal = () => {
-    const array = display.split(" ");
-    const lastElement = array[array.length - 1];
-    if (!lastElement.includes(".") && isNaN(parseInt(lastElement)) === false) {
-      setDisplay(display + ".");
+  const calculate = () => {
+    if (/[\+\-\*\/]/.test(display)) {
+      const answer = parseFloat(evaluate(display).toFixed(12));
+      setDisplay(answer);
     }
+
+    // setDisplay(parseFloat(evaluate(display).toFixed(12)).toString());
   };
 
   return (
@@ -41,14 +56,13 @@ function App() {
       <main className="h-screen flex justify-center items-center">
         <div className=" bg-neutral-950 flex flex-col items-center justify-center rounded-md shadow-[5px_5px_5px_#000000]">
           <div className="w-[300px] h-[70px] border-b border-neutral-600 m-3 flex justify-end items-end overflow-hidden">
-            <span id="display" className="text-4xl pb-2">
+            <h1 id="display" className="text-4xl pb-2">
               {display}
-            </span>
+            </h1>
           </div>
           <div className="w-[300px] h-[370px] grid grid-rows-5 grid-cols-4 gap-3 m-3 text-2xl">
             <button
               id="clear"
-              value=""
               onClick={() => {
                 setDisplay("0");
               }}
@@ -58,128 +72,112 @@ function App() {
             </button>
             <button
               id="divide"
-              value=""
-              onClick={handleOperators}
+              onClick={() => handleDisplay("/")}
               className="bg-[#F69906] rounded-full text-2xl hover:bg-[#e9bc73] transition duration-200 ease-in"
             >
               /
             </button>
             <button
               id="seven"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("7")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               7
             </button>
             <button
               id="eight"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("8")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               8
             </button>
             <button
               id="nine"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("9")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               9
             </button>
             <button
               id="multiply"
-              value=""
-              onClick={handleOperators}
+              onClick={() => handleDisplay("*")}
               className="bg-[#F69906] rounded-full text-2xl hover:bg-[#e9bc73] transition duration-200 ease-in"
             >
               *
             </button>
             <button
               id="four"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("4")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               4
             </button>
             <button
               id="five"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("5")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               5
             </button>
             <button
               id="six"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("6")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               6
             </button>
             <button
               id="subtract"
-              value=""
-              onClick={handleOperators}
+              onClick={() => handleDisplay("-")}
               className="bg-[#F69906] rounded-full hover:bg-[#e9bc73] transition duration-200 ease-in"
             >
               -
             </button>
             <button
               id="one"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("1")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               1
             </button>
             <button
               id="two"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("2")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               2
             </button>
             <button
               id="three"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("3")}
               className="bg-[#313131] rounded-full hover:bg-[#575757] transition duration-200 ease-in"
             >
               3
             </button>
             <button
               id="add"
-              value=""
-              onClick={handleOperators}
+              onClick={() => handleDisplay("+")}
               className="bg-[#F69906] rounded-full hover:bg-[#e9bc73] transition duration-200 ease-in"
             >
               +
             </button>
             <button
               id="zero"
-              value=""
-              onClick={handleNumbers}
+              onClick={() => handleDisplay("0")}
               className="bg-[#313131] rounded-full col-span-2  hover:bg-[#575757] transition duration-200 ease-in"
             >
               0
             </button>
             <button
               id="decimal"
-              value=""
-              onClick={handleDecimal}
+              onClick={() => handleDisplay(".")}
               className="bg-[#313131] rounded-full  hover:bg-[#575757] transition duration-200 ease-in"
             >
               .
             </button>
             <button
               id="equals"
-              value=""
-              onClick={handleEqual}
+              onClick={calculate}
               className="bg-[#F69906] rounded-full hover:bg-[#e9bc73] transition duration-200 ease-in"
             >
               =
